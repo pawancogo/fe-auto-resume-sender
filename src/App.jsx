6f27e4a2653ae7028a0b6250318105e81acd006a
Pawan Kumar <pawan.kumar2@builder.ai>
2025-04-11 16:18:57 +0530

    initial setup and tailswind config

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+
+      <div class="text-5xl font-bold underline">
+        Hello world!
+      </div>
+      
+    </>
+  )
+}
+
+export default App
